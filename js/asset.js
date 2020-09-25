5d845f9681605d28c7db0fc1d60a467d0950bb83
Mark Corpuz <social@markcorpuz.com>
2020-09-25 08:14:26 -0700

    Testing red background color on ratio

--- js/asset.js
+++ js/asset.js
@@ -45,7 +45,7 @@
 	function AppendVideo( yt_ids ) {
 	    
 	    $( 'div#vimeo_' + yt_ids )
-	        .append( '<div style="padding:56.25% 0 0 0;position:relative;">' +
+	        .append( '<div style="padding:56.25% 0 0 0;position:relative;background-color:red;">' +
 	        			'<iframe src="https://player.vimeo.com/video/' + yt_ids + '?autoplay=1&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>' +
 	                 '</div><script src="https://player.vimeo.com/api/player.js"></script>' );
 	    
